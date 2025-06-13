dea922cc9ccff34313fee2d0f16a6aba69798701
klos02 <bartoszklosinski02@gmail.com>
2025-06-14 01:42:34 +0200

    create app db context

---
+++ EmailOrderApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using System;
+using EmailOrderApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailOrderApp.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+}
