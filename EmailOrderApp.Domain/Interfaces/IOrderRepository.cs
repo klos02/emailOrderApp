f90925ecd9096465b836d24c83d801bf5e8d2196
klos02 <bartoszklosinski02@gmail.com>
2025-06-14 03:04:12 +0200

    add order repository interface

---
+++ EmailOrderApp.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using EmailOrderApp.Domain.Entities;
+
+namespace EmailOrderApp.Domain.Interfaces;
+
+public interface IOrderRepository
+{
+    Task<Order?> GetByOrderNumberAsync(int OrderNumber);
+    Task<IEnumerable<Order>> GetAllAsync();
+    Task<bool> AddAsync(Order order);
+}
